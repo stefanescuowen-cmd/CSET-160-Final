386837a706f66e8acfda2bae5c7089f06eced089
Owen <stefanescuowen@gmail.com>
2026-03-28 15:16:02 -0400

    Went through and tried to fix OneDrive error causing files to not upload.

---
+++ app/models/submission.py
@@ -0,0 +1,18 @@
+from app.extensions import db
+
+class Submission(db.Model):
+    __tablename__ = "submissions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    test_id = db.Column(db.Integer, db.ForeignKey("tests.id"))
+    student_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    marks = db.Column(db.Integer)
+    graded_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+    submitted_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    is_late = db.Column(db.Boolean, default=False)
+
+    # Relationships
+    student = db.relationship("User", back_populates="submissions", foreign_keys=[student_id])
+    grader = db.relationship("User", back_populates="graded_submissions", foreign_keys=[graded_by])
+    test = db.relationship("Test", back_populates="submissions")
+    answers = db.relationship("Answer", back_populates="submission", lazy=True)
