fa5d2f3e52bbae457c597270137aadffe18636c1
Davion <davionwagstaff@gmail.com>
2026-03-30 08:38:03 -0400

    Design

--- main.py
+++ main.py
@@ -226,4 +226,4 @@ def test_summary(test_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
